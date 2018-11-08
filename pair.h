52bb419444608426acb0aee1c5e50c96b9b642af
Stephen Crane <cranegames@gmail.com>
2018-11-07 21:48:16 -0800

    Base templated Pair structure

--- pair.h
+++ pair.h
@@ -3,13 +3,30 @@
 
 #include <iostream>
 
+using namespace std;
+
 template<typename K, typename V>
 struct Pair{
     K key;
     V val;
-
+    //CTOR
     Pair(const K& k=K(), const V& v=V());
-    friend std::ostream& operator <<(std::ostream& outs, Pair<K,V>& pair);
+    //COMPARISON OPERATORS
+    Pair<K,V>& operator =(const V& right);
+    operator V() const;
+    template<typename M, typename N>
+    friend bool operator ==(const Pair<M,N>& lhs, const Pair<M,N>& rhs);
+    template<typename M, typename N>
+    friend bool operator < (const Pair<M,N>& lhs, const Pair<M,N>& rhs);
+    template<typename M, typename N>
+    friend bool operator > (const Pair<M,N>& lhs, const Pair<M,N>& rhs);
+    template<typename M, typename N>
+    friend bool operator <= (const Pair<M,N>& lhs, const Pair<M,N>& rhs);
+    //Addition operator
+    template<typename M, typename N>
+    friend Pair<M,N>& operator + (const Pair<M,N>& lhs, const Pair<M,N>& rhs);
+    template<typename M, typename N>
+    friend ostream& operator <<(ostream& , const Pair<M,N>& );
 };
 
 template<typename K, typename V>
@@ -18,10 +35,44 @@ Pair<K,V>::Pair(const K &k, const V &v)
 
 }
 template<typename K, typename V>
-std::ostream& operator <<(std::ostream& outs, Pair<K,V>& pair){
-    outs << "(" << key << ": " << val << ")";
+bool operator ==(const Pair<K, V>& lhs, const Pair<K, V>& rhs){
+    //Compares if the keys are equivalent
+    return lhs.key == rhs.key;
+}
+template<typename K, typename V>
+bool operator < (const Pair<K, V>& lhs, const Pair<K, V>& rhs){
+    return lhs.key < rhs.key;
+}
+template<typename K, typename V>
+bool operator > (const Pair<K, V>& lhs, const Pair<K, V>& rhs){
+    return lhs.key > rhs.key;
+}
+template<typename K, typename V>
+bool operator <= (const Pair<K, V>& lhs, const Pair<K, V>& rhs){
+    return lhs.key <= rhs.key;
+}
+template<typename K, typename V>
+Pair<K, V>& operator + (const Pair<K, V>& lhs, const Pair<K, V>& rhs){
+    //Adds the lhs/rhs values
+    Pair<K,V>* pair = new Pair<K,V>;
+    pair->key = lhs.key;
+    pair->val = lhs.val;
+    pair->val += rhs.val;
+    return (*pair);
+}
+template<typename K, typename V>
+std::ostream& operator <<(std::ostream& outs, const Pair<K,V>& print){
+    outs << "(" << print.key << ": " << print.val << ")";
     return outs;
 }
-
+template<typename K, typename V>
+Pair<K,V>& Pair<K,V>::operator =(const V& right){
+    this->val = right;
+    return (*this);
+}
+template<typename K, typename V>
+Pair<K,V>::operator V() const{
+    return this->val;
+}
 
 #endif // PAIR_H
