b27a59456fe0807af17e6e072359a289760f887e
Stephen Crane <cranegames@gmail.com>
2018-11-08 10:31:32 -0800

    Updated overloaded +()

--- pair.h
+++ pair.h
@@ -2,6 +2,7 @@
 #define PAIR_H
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -11,8 +12,10 @@ struct Pair{
     V val;
     //CTOR
     Pair(const K& k=K(), const V& v=V());
-    //COMPARISON OPERATORS
+    //MODIFIERS
     Pair<K,V>& operator =(const V& right);
+    //COMPARISON OPERATORS
+
     operator V() const;
     template<typename M, typename N>
     friend bool operator ==(const Pair<M,N>& lhs, const Pair<M,N>& rhs);
@@ -24,7 +27,7 @@ struct Pair{
     friend bool operator <= (const Pair<M,N>& lhs, const Pair<M,N>& rhs);
     //Addition operator
     template<typename M, typename N>
-    friend Pair<M,N>& operator + (const Pair<M,N>& lhs, const Pair<M,N>& rhs);
+    friend Pair<M,N> operator + (const Pair<M,N>& lhs, const Pair<M,N>& rhs);
     template<typename M, typename N>
     friend ostream& operator <<(ostream& , const Pair<M,N>& );
 };
@@ -52,13 +55,13 @@ bool operator <= (const Pair<K, V>& lhs, const Pair<K, V>& rhs){
     return lhs.key <= rhs.key;
 }
 template<typename K, typename V>
-Pair<K, V>& operator + (const Pair<K, V>& lhs, const Pair<K, V>& rhs){
+Pair<K, V> operator + (const Pair<K, V>& lhs, const Pair<K, V>& rhs){
     //Adds the lhs/rhs values
-    Pair<K,V>* pair = new Pair<K,V>;
-    pair->key = lhs.key;
-    pair->val = lhs.val;
-    pair->val += rhs.val;
-    return (*pair);
+//    Pair<K,V>* pair = new Pair<K,V>;
+//    pair->key = lhs.key;
+//    pair->val = lhs.val;
+//    pair->val += rhs.val;
+    return Pair<K,V>(lhs.key, (lhs.val + rhs.val));
 }
 template<typename K, typename V>
 std::ostream& operator <<(std::ostream& outs, const Pair<K,V>& print){
